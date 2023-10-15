79c82c43ed2fef0571cffb66e1258ca181a67dd0
Md Morsalin Hossain <mdmorsalin80@gmail.com>
2023-10-15 20:05:47 +0600

    [WelcomePage] Adding Welcome page Problem : None Solution: - added welcome page UI - added Login and registration button - added firebase
    Signed-off-by: Md Morsalin Hossain <mdmorsalin80@gmail.com>

--- app/src/main/java/com/example/mealmanagement/MainActivity.kt
+++ app/src/main/java/com/example/mealmanagement/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.mealmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnLogin: Button
+    lateinit var btnRegister: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById(R.id.loginBtn)
+        btnRegister = findViewById(R.id.RegisterBtn)
+
+        btnLogin.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Login Button pressed", Toast.LENGTH_LONG).show()
+        })
+
+        btnRegister.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Register Button pressed", Toast.LENGTH_LONG).show()
+        })
     }
 }
